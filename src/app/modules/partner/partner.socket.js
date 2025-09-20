4ceeb1f9b02032611b072668ccebbd20068a8c08
TayeburRahman <tayebrayhan101+@gmail.com>
2025-09-20 17:11:54 +0600

    updates all

--- src/app/modules/partner/partner.socket.js
+++ src/app/modules/partner/partner.socket.js
@@ -2,65 +2,82 @@ const httpStatus = require("http-status");
 const ApiError = require("../../../errors/ApiError");
 const { ENUM_SOCKET_EVENT, ENUM_USER_ROLE } = require("../../../utils/enums");
 const Services = require("../services/services.model");
-// const Order = require("../order/order.model");
-const Partner = require("./partner.model"); 
+const Partner = require("./partner.model");
 
 const handlePartnerData = async (receiverId, role, socket, io) => {
+  // update Partner information of location
+  socket.on(ENUM_SOCKET_EVENT.PARTNER_LOCATION, async (data) => {
+    try {
+      const { latitude, longitude, address } = data || {};
 
-    // update Partner information of location 
-    socket.on(ENUM_SOCKET_EVENT.PARTNER_LOCATION, async (data) => {
-
-        const { latitude, longitude, address } = data; 
-        console.log("====", latitude, longitude)
-
-        if (!receiverId || !data?.longitude || !data?.latitude) { 
-            socket.emit('error', {
-                message: 'longitude or latitude not found',
-            });
-            return;
-        }
-
-        const updatedPartner = await Partner.findByIdAndUpdate(
-            receiverId,
-            { $set: { "location.coordinates": [Number(longitude), Number(latitude)] } },
-            {
-                new: true,
-                runValidators: true,
-                projection: { location: 1 },
-            }
-        );  
-
-        if (!updatedPartner) {
-            throw new ApiError(httpStatus.NOT_FOUND, 'Partner not found!');
-            return;
-        }
-
-        const { location } = updatedPartner;
-
-        const services = await Services.find({
-            confirmedPartner: receiverId,
-            status: { $in: ["accepted", "rescheduled", "pick-up", "in-progress"] },
+      if (!receiverId || !longitude || !latitude) {
+        return socket.emit("error", {
+          status: httpStatus.BAD_REQUEST,
+          message: "longitude or latitude not found",
         });
+      }
 
-        if (services && services?.length) {
-
-            services.forEach(service => {
-                const { _id, confirmedPartner } = service;
-                const updateLocationPartner = {
-                    partnerId: confirmedPartner,
-                    serviceId: _id,
-                    location,
-                }
-                const userId = service.user.toString();
-                io.to(userId).emit(ENUM_SOCKET_EVENT.PARTNER_LOCATION, updateLocationPartner);
-            });
+      const updatedPartner = await Partner.findByIdAndUpdate(
+        receiverId,
+        {
+          $set: {
+            "location.coordinates": [Number(longitude), Number(latitude)],
+            ...(address && { "location.address": address }), // optional address update
+          },
+        },
+        {
+          new: true,
+          runValidators: true,
+          projection: { location: 1 },
         }
-        io.to(receiverId.toString()).emit(ENUM_SOCKET_EVENT.PARTNER_LOCATION, location);
-    },
-    );
+      );
 
+      if (!updatedPartner) {
+        return socket.emit("error", {
+          status: httpStatus.NOT_FOUND,
+          message: "Partner not found!",
+        });
+      }
+
+      const { location } = updatedPartner;
+
+      // find active services linked to this partner
+      const services = await Services.find({
+        confirmedPartner: receiverId,
+        status: { $in: ["accepted", "rescheduled", "pick-up", "in-progress"] },
+      });
+
+      if (services?.length) {
+        services.forEach((service) => {
+          const { _id, confirmedPartner, user } = service;
+          const updateLocationPartner = {
+            partnerId: confirmedPartner,
+            serviceId: _id,
+            location,
+          };
+          io.to(user.toString()).emit(
+            ENUM_SOCKET_EVENT.PARTNER_LOCATION,
+            updateLocationPartner
+          );
+        });
+      }
+
+      // also send back to the partner himself
+      io.to(receiverId.toString()).emit(
+        ENUM_SOCKET_EVENT.PARTNER_LOCATION,
+        location
+      );
+    } catch (error) {
+      console.error("Socket error (PARTNER_LOCATION):", error);
+
+      socket.emit("error", {
+        status: error.statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message || "Something went wrong while updating partner location",
+      });
+    }
+  });
 };
 
 module.exports = {
-    handlePartnerData,
-}
+  handlePartnerData,
+};
